d328845411a9385edb159bff9d479157c280b308
Naufal Aulia <naufal7600@gmail.com>
2024-05-24 19:58:10 +0700

    Add Basic Ground Tile Mapping, Player Character Controller and Simple Player Animation

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator _animator;
+    private Rigidbody2D _rb;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    private void FixedUpdate()
+    {
+        _animator.SetFloat("deltaX", _rb.velocity.normalized.x);
+        _animator.SetFloat("deltaY", _rb.velocity.normalized.y);
+    }
+}
